f8f25028474bdd542c60228670ddedf065e09a7b
Misa Todorovic <misa.todorovic@sipodsoftware.com>
2026-03-11 17:21:11 +0100

    Added env variables

--- GameHubz.Api/Program.cs
+++ GameHubz.Api/Program.cs
@@ -36,6 +36,8 @@ namespace GameHubz.Api
             // Add services to the container.
             builder.Services.AddControllers();
 
+            builder.Configuration.AddEnvironmentVariables();
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
 
